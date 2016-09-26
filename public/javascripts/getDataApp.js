c5b8d2660be6e0a0e755e454cc766bcffc55f2e0
Vikram Gopali <vgopali@yodlee.com>
2016-09-27 02:50:08 +0530

    unformatted search result display

--- public/javascripts/getDataApp.js
+++ public/javascripts/getDataApp.js
@@ -4,15 +4,23 @@
  */
 
 var app = angular.module('myApp', []);
+
 app.controller('myCtrl', ['$scope','$http',function ($scope,$http) {
+    $scope.searchResult=[];
     $scope.submit = function(firstname){
         $http.post('/getData',{ "name": firstname }).success( function(response) {
-            console.log("success - ", response.data);
+            //console.log("success - ", response.data);
+            angular.forEach(response.data,function (item) {
+                $scope.searchResult.push(item);
+                //console.log(item);
+            });
+            console.log($scope.searchResult);
         })
         .error( function(response) {
             console.log("error");
         });
 
-
     };
 }]);
+
+
